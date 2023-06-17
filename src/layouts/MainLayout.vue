a3fea262a73da79892bb4200961bb18f70546a90
Hans Dorrestijn <hans.dorrestijn@politie.nl>
2023-06-17 13:44:07 +0200

    Remove "Quasar App" text

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,8 +2,8 @@
     <q-layout view="lHh Lpr lFf">
         <q-header elevated>
             <q-toolbar>
-                <q-toolbar-title> Quasar App </q-toolbar-title>
-                <div>Quasar v{{ $q.version }}</div>
+                <q-toolbar-title> Live Frame App </q-toolbar-title>
+                <div>Versie v0.0.1</div>
             </q-toolbar>
         </q-header>
 
